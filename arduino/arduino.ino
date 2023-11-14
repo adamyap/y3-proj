0b3f9b6842b9cec0a42f307de15e6f94ba9b83ff
Adam Yap <kn21951@bristol.ac.uk>
2023-11-14 16:44:00 +0000

    it moves

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -2,12 +2,12 @@
 
 Servo servo1;  // create servo object to control a servo
 Servo servo2;
-int pos1 = 90;    // variable to store the servo position
-int pos2 = 90;
+int pos1 = 100;    // variable to store the servo position
+int pos2 = 100;
 
 void setup() {
-  servo1.attach(6);  // attaches the servo on pin 6 to the servo object
-  servo2.attach(5);
+  servo1.attach(11);  // attaches the servo on pin 6 to the servo object
+  servo2.attach(3);
   Serial.begin(9600);  // opens serial port, sets data rate to 9600 bps
   Serial.setTimeout(10);
   servo1.write(pos1);  // move the servo to the initial position
@@ -19,13 +19,13 @@ void loop() {
     int val1 = Serial.parseInt();  // read the incoming number for servo1
     if (Serial.read() != ',') return;  // check for the comma
     int val2 = Serial.parseInt();  // read the incoming number for servo2
-    if (val1 >= 0 && val1 <= 100) {  // check if the value is within the expected range
+    if (val1 >= 0 && val1 <= 120) {  // check if the value is within the expected range
       pos1 = val1 + 40;  // add 40 to the value
       servo1.write(pos1);  // tell servo1 to go to the position
       //Serial.print(val1);
       //Serial.println(" degrees for servo1.");
     }
-    if (val2 >= 0 && val2 <= 100) {  // check if the value is within the expected range
+    if (val2 >= 0 && val2 <= 120) {  // check if the value is within the expected range
       pos2 = val2 + 40;  // add 40 to the value
       servo2.write(pos2);  // tell servo2 to go to the position
       //Serial.print(val2);
--- initial_demonstration.py
+++ initial_demonstration.py
@@ -5,19 +5,72 @@ ser = serial.Serial('COM11', 9600)  # replace 'COM11' with the port where your A
 time.sleep(2)  # wait for the serial connection to initialize
 
 def move_servos(angle1, angle2):
-    if 0 <= angle1 <= 100 and 0 <= angle2 <= 100:
+    if 0 <= angle1 <= 120 and 0 <= angle2 <= 120:
         ser.write(f"{angle1},{angle2}".encode())
         #time.sleep(1)  # wait for the servos to move
 
 def continuous_rotation():
-    move_servos(100, 100)
+    move_servos(120, 120)
     time.sleep(1)  # adjust this delay for speed control
     move_servos(0, 0)
     time.sleep(1)
 
-while True:
-    #angle1 = int(input("Enter an angle for servo1 (0-100): "))
-    #angle2 = int(input("Enter an angle for servo2 (0-100): "))
-    #move_servos(angle1, angle2)
-    continuous_rotation()
+def simple_maze():
+    move_servos(60, 60)
+    time.sleep(2)
+    move_servos(0, 0)
+    time.sleep(1.5)
+    move_servos(120, 120)
+    time.sleep(1)
+    move_servos(120, 0)
+    time.sleep(1)
+    move_servos(0, 120)
+    time.sleep(1)
+    move_servos(0, 0)
+    time.sleep(1)
+    move_servos(120, 0) ####
+    time.sleep(0.23)
+    move_servos(0, 30)
+    time.sleep(2)
+    move_servos(120, 120)
+    time.sleep(1)
+    move_servos(120, 0)
+    time.sleep(1)
+    move_servos(120, 120)
+    time.sleep(1)
+    move_servos(0, 50)
+    time.sleep(1)
+    move_servos(0, 120)
+    time.sleep(1)
+    move_servos(120, 120)
+    time.sleep(1)
+    move_servos(30, 120)
+    time.sleep(1)
+    move_servos(120, 120)
+    time.sleep(1)
+    move_servos(120, 0)
+    time.sleep(1)
+    move_servos(0, 0)
+    time.sleep(1)   
+    move_servos(120, 0)
+    time.sleep(1)
+    move_servos(0, 0)
+    time.sleep(1)
+    move_servos(120, 0)
+    time.sleep(1)
+    move_servos(0, 0)
+    time.sleep(1)
+    move_servos(0, 120)
+    time.sleep(1)
+    move_servos(0, 0)
+    time.sleep(1)
+    move_servos(60, 60)
 
+
+while True:
+    #angle1 = int(input("Enter an angle for servo1 (0-120): "))
+    #angle2 = int(input("Enter an angle for servo2 (0-120): "))
+    #move_servos(angle1, angle2)
+    #continuous_rotation()
+    simple_maze()
+    break
