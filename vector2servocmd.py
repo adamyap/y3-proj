dcc2483a7f27cb870d3b71ff436fd46dad4aa10c
Salt9001 <151855719+Salt9001@users.noreply.github.com>
2024-02-02 14:14:38 +0000

    Updated vector2servocmd to vector2servocmd.py

---
+++ vector2servocmd.py
@@ -0,0 +1,27 @@
+import math 
+startup_angle = 10
+scale=50
+ball_coordx1 = int(input("insert ball coordx1:"))
+ball_coordy1 = int(input("insert ball coordy1:"))
+ball_coordx2 = int(input("insert ball coordx2:"))
+ball_coordy2 = int(input("insert ball coordy2:"))
+velocity = int(input("insert moving speed in px/s:"))
+distancex = ball_coordx2 - ball_coordx1 # east/west for rotor 1(longer edge)
+distancey = ball_coordy2 - ball_coordy1 #south/north for rotor 2(shorter edge)
+angle = math.atan(distancey/distancex)
+
+if distancex >0:
+    Ranglex = startup_angle+scale*(abs(math.cos(angle)))
+elif distancex == 0:
+    Ranglex = 0
+else:
+    Ranglex = -startup_angle-scale*(abs(math.cos(angle)))
+if distancey >0:
+    Rangley = startup_angle+scale*(abs(math.sin(angle)))
+elif distancey == 0:
+    Rangley =0
+else:
+    Rangley = -startup_angle-scale*(abs(math.sin(angle)))
+print("angle =:",math.degrees(angle))
+print("rotorx angle =:",round(Ranglex,2),"rotory angle =:",round(Rangley,2))
+
