a989166c53dc76192692dade4138fc2fc46cd07e
Adam Yap <kn21951@bristol.ac.uk>
2024-04-26 14:23:30 +0100

    update main4.py

--- main/dual_motor_control_via_serial/main4.py
+++ main/dual_motor_control_via_serial/main4.py
@@ -36,45 +36,33 @@ def update_timer():
         timer_label.after(50, update_timer)
 
 def define_path(contours,edges):
-    # Define the minimum area for a filled contour
+    # min area for filled path
     min_area = 2000
     min_perimeter = 5000
-
-    # Filter contours based on the area
+    # filter contours based on area and perimeter to highlight path
     filled_contours = [cnt for cnt in contours if cv2.contourArea(cnt) > min_area and cv2.arcLength(cnt, True) > min_perimeter]
-
-    # Create an empty mask to draw the filled contours
     mask = np.zeros_like(edges)
-
-    # Draw the filled contours on the mask
     for cnt in filled_contours:
         cv2.drawContours(mask, [cnt], -1, (255), thickness=cv2.FILLED)
-
-    #cv2.imshow('path',mask)
-
-    # Skeletonize the mask
+    # skeletonize mask to get continuous line of points
     skeleton = skeletonize(mask // 255)
-    skeleton = img_as_ubyte(skeleton)  # Convert the image back to 8-bit
-    
-    # Get the coordinates of the skeleton path
+    skeleton = img_as_ubyte(skeleton)
+    # get points from skeleton
     y, x = np.where(skeleton == 255)
-
-    # Create an array of evenly spaced indices
-    indices = np.linspace(0, len(x) - 1, 100,dtype=int) #number of checkpoints
-
-    # Sample the x and y arrays with these indices
+    # create array of evenly-spaced indices
+    indices = np.linspace(0, len(x) - 1, 100,dtype=int) #100: number of checkpoints
+    # sample x & y array with indices
     sample_x = x[indices]
     sample_y = y[indices]
-
     points = list(zip(sample_x, sample_y))
 
-    # Find contours in the skeleton
+    # for contours in skeleton, find geometric approximation of path
     contours, _ = cv2.findContours(skeleton, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
     for contour in contours:
         epsilon = 0.001*cv2.arcLength(contour,True)
         approx = cv2.approxPolyDP(contour,epsilon,True)
         for point in approx:
+            # if point is not close to an existing point, add it to the list
             distances = [np.sqrt((point[0][0] - p[0])**2 + (point[0][1] - p[1])**2) for p in points]
             if np.min(distances) > 10:
                 points.append(tuple(point[0]))
@@ -82,28 +70,19 @@ def define_path(contours,edges):
     return points, contours
 
 def order_points(image,points):
-
     lower_yellow = np.array([20, 180, 80])
     upper_yellow = np.array([30, 255, 255])
 
-    # Convert the image to HSV color space
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-    # Create a binary mask where the color is within the range
     mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
-
-        # Find contours in the mask
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    # Find the largest contour, assuming it's the yellow object
     largest_contour = max(contours, key=cv2.contourArea)
-
-    # Calculate the center of the largest contour
+    # center of largest contour
     M = cv2.moments(largest_contour)
     cX = int(M["m10"] / M["m00"])
     cY = int(M["m01"] / M["m00"])
     ordered_points = [[cX,cY]]
-
+    # find nearest point to the center, order them by distance
     while points:
         last_point = ordered_points[-1]
         distances = [np.sqrt((point[0] - last_point[0])**2 + (point[1] - last_point[1])**2) for point in points]
@@ -113,88 +92,42 @@ def order_points(image,points):
     return ordered_points
 
 def define_hole(contours,edges):
-    # Define the min and max area for a filled contour
     min_area = 100
     max_area = 10000
-
-    # Filter contours based on the area
     circle_contours = [cnt for cnt in contours if cv2.contourArea(cnt) > min_area and cv2.contourArea(cnt) < max_area]
-
-    # Create an empty mask to draw the filled contours
     mask = np.zeros_like(edges)
-
-    # Draw the filled contours on the mask
     for cnt in circle_contours:
         cv2.drawContours(mask, [cnt], -1, (255), thickness=cv2.FILLED)
 
-    #cv2.imshow('hole',mask)
-
-    # Set up the SimpleBlobDetector parameters
     params = cv2.SimpleBlobDetector_Params()
-
     params.filterByColor = True
     params.blobColor = 255
     params.filterByCircularity = True
     params.minCircularity = 0.75
-
-    # Create a detector with the parameters
     detector = cv2.SimpleBlobDetector_create(params)
-
-    # Detect blobs
     keypoints = detector.detect(mask)
 
     return keypoints
 
-def define_wall(contours):
-    # Define the minimum area for a filled contour
-    max_area = 1000
-
-    # Filter contours based on the area
-    wall_contours = [cnt for cnt in contours if cv2.contourArea(cnt) < max_area]
-
-    return wall_contours
-
 def locate_ball(frame,edges):
-    # Convert the frame to HSV
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    #Threshold the HSV image to get only red colors
-    # lower_red1 = np.array([0, 120, 60])
-    # upper_red1 = np.array([10, 255, 255])
-    # lower_red2 = np.array([170, 120, 60])
-    # upper_red2 = np.array([180, 255, 255])
-
     lower_red1 = np.array([0, 60, 70])
     upper_red1 = np.array([5, 200, 210])
     lower_red2 = np.array([170, 80, 80])
     upper_red2 = np.array([180, 190, 200])
 
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
     mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
     mask = cv2.bitwise_or(mask1, mask2)
-
-    # Find contours in the mask
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    # Define the minimum area for a filled contour
     min_area = 100
-
-    # Filter contours based on the area
     ball_contours = [cnt for cnt in contours if cv2.contourArea(cnt) > min_area]
-
-    # Create an empty mask to draw the filled contours
     mask = np.zeros_like(edges)
-
-    # Draw the filled contours on the mask
     cv2.drawContours(mask, ball_contours, -1, (255), thickness=cv2.FILLED)
-
-    #cv2.imshow('Ball Mask',mask)
-
-    # Merge all the contours into one
+    # merge all contours
     if ball_contours:
         merged_contour = np.concatenate([contour.reshape(-1, 2) for contour in ball_contours])
-
-        # Calculate the convex hull of the merged contour
+        # find convex hull of contour
         hull = cv2.convexHull(merged_contour)
     else:
         hull = None
@@ -202,16 +135,11 @@ def locate_ball(frame,edges):
     return hull
 
 def define_end(frame):
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    # Define the color range for the blue marking
     lower_blue = np.array([100, 150, 100])
     upper_blue = np.array([110, 255, 255])
 
-    # Threshold the HSV image to get only blue colors
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
-
-    # Find contours in the mask
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     return contours
@@ -222,38 +150,32 @@ def run_image_processing():
     global start_solve
     global ser
     global end_reached
+    global interval_time
     try:
-        # Initialize serial connection
         ser = serial.Serial('COM3', 9600)
-        # Connect to webcam (0 = default cam)
-        cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
-        # Set the resolution to 720p
+        cap = cv2.VideoCapture(0,cv2.CAP_DSHOW) # 0 - default cam
         cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
         cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-        # Set the frame rate to 60fps
         cap.set(cv2.CAP_PROP_FPS, 30)
         send_position(0,0)
     except:
         print("Error: Could not connect to the camera or the Arduino")
         pass
 
-    # Start the timer
     start_time = time.time()
     checkpoint_time = time.time()
-    # Flag to indicate if the image has been captured
     image_captured = False
 
-    # Backup image
     backup_image = cv2.imread('green.jpg')
     cv2.resize(backup_image,(1280,720))
 
     prev_center = None
     velocity = [0, 0]
-    N = 1 # Number of frames to consider for the moving average
+    N = 1 # no. frames for speed moving average
     velocities = []
     prev_time = time.time()
-    KpX,KiX,KdX = 0.1,0.2,0.03 #0.1 0.2 0.03
-    KpY,KiY,KdY = 0.2,0.2,0.03 #0.2 0.2 0.03
+    KpX,KiX,KdX = 0.25,0.25,0.030 #0.1 0.2 0.03
+    KpY,KiY,KdY = 0.31,0.25,0.030 #0.2 0.2 0.03
     x_integral = 0
     y_integral = 0
     x_distance_prev = 0
@@ -271,36 +193,36 @@ def run_image_processing():
         except:
             frame = rectify(backup_image)
 
+        # take initial picture with delay to allow cam to start & settle
         if not image_captured and time.time() - start_time >= 1.5:
             image = frame
             gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
             blurred = cv2.GaussianBlur(gray, (5, 5), 0)
             edges = cv2.Canny(blurred, 15, 50)
             contours, _ = cv2.findContours(edges.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-            # Define walls
-            #wall_contours = define_wall(contours)
-            #for cnt in wall_contours:
-            #    cv2.drawContours(image, [cnt], -1, (255, 155, 0), 1)
-            # Define and draw holel
+
+            # draw holes
             keypoints = define_hole(contours,edges)
             cv2.drawKeypoints(image, keypoints, image, (0,255,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-            # Define and draw path
+            # draw & order path
             path_points,path_contours = define_path(contours,edges)
             ordered_points = order_points(image, path_points)
-            # Separate path points into x and y arrays
+            # separate x & y coords of path
             x_path = np.array([point[0] for point in ordered_points])
             y_path = np.array([point[1] for point in ordered_points])
-            # Label each point on the path
+            # label each point on path to show order
             for i,point in enumerate(ordered_points):
                 cv2.circle(image, tuple(map(int, point)), 3, (255, 255, 255), -1)
                 cv2.putText(image, str(i), (point[0] + 5, point[1] + 5), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 255, 0), 1)
-            # Draw the path contour
+            # draw path contour
             for contour in path_contours:
                 cv2.drawContours(image, [contour], -1, (0, 255, 0), 1)
             end_contours = define_end(image)
+            # draw end contour
             for cnt in end_contours:
                 cv2.drawContours(image, [cnt], -1, (255, 0, 0), -1)
 
+            # save resultant processed image so we don't overwrite original frame in following code
             processed_image = image.copy()
             cv2.imshow('Working Image', image)
             image_captured = True
@@ -310,84 +232,69 @@ def run_image_processing():
             if ball_contours is not None:
                 image = processed_image.copy()
 
-                # Calculate moments of the contours
+                # find centre of ball
                 M = cv2.moments(ball_contours)
-
-                # Calculate the center of the contours
                 center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-
-                # Draw the smallest enclosing circle on the frame
+                # draw a circle around ball to represent on image
                 cv2.circle(image, center, 13, (0, 0, 255), thickness=2)
-
-                # Draw crosshair at the center
+                # draw crosshair at centre of ball
                 cv2.line(image, (center[0] - 5, center[1]), (center[0] + 5, center[1]), (0, 0, 255), 1)
                 cv2.line(image, (center[0], center[1] - 5), (center[0], center[1] + 5), (0, 0, 255), 1)
 
-                # Calculate velocity
+                # calculate ball velocity
                 if prev_center is not None:
                     prev_center = (int(center[0] + prev_center[0])*0.5 , int(center[1] + prev_center[1])*0.5) #qiuck fix
-
                     dx = center[0] - prev_center[0]
                     dy = center[1] - prev_center[1]
-                    dt = time.time() - prev_time  # Time difference in seconds
+                    dt = time.time() - prev_time  # time between frames
                     velocities.append((dx/dt, dy/dt))
                     if len(velocities) > N:
                         velocities.pop(0)
                     velocity = [sum(v[i] for v in velocities) / len(velocities) for i in range(2)]
                 prev_center = center
                 prev_time = time.time()
-           
-                # Draw velocity vector
-                scale = 0.1  # Adjust this value to change the scale of the velocity vector
+                # draw a green vector with scale & direction to represent velocity
+                scale = 0.1
                 end_point = (int(center[0] + velocity[0]*scale), int(center[1] + velocity[1]*scale))
                 cv2.arrowedLine(image, center, end_point, (0, 255, 0), 2)
             
             if start_solve == True:
-
-                # Check if the center of the ball is within the end
+                # check if ball is at the end of the course
                 for contour in end_contours:
                     if cv2.pointPolygonTest(contour, (center[0], center[1]), False) >= 0:
                         print("Congratulations! The ball has reached the end of the maze.")
                         end_reached = True
                         break
-                
                 if not end_reached:
                     if x_path.size == 0 or y_path.size == 0:
                         print("Congratulations! The ball has reached the end of the maze.")
                         end_reached = True
                         break
 
-                # The nearest point on the path is then
+                # find the nearest point on the path to the ball
                 x_nearest, y_nearest = x_path[0], y_path[0]
-
-                # Draw a line from the point to the nearest point on the path
+                # draw line to this point
                 cv2.line(image, (center[0], center[1]), (x_nearest, y_nearest), (0, 255, 0), 1)
-
-                # Calculate the Euclidean distance to nearest point
+                # distance from ball to nearest point
                 euclidean_distance = np.sqrt((x_nearest - center[0])**2 + (y_nearest - center[1])**2)
-
-                # Calculate the Euclidean distance to all points
+                # distance from ball to all nearest point
                 euclidean_distances = np.array([np.sqrt((x - center[0])**2 + (y - center[1])**2) for x, y in zip(x_path, y_path)])
-
-                # Display the Euclidean distance next to the line
+                # show distance next to drawn line
                 cv2.putText(image, f'Distance: {euclidean_distance:.2f}', (center[0], center[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
 
-                # Calculate the x and y distances
                 x_distance = (x_nearest - center[0])
                 y_distance = -(y_nearest - center[1])
-
-                # Display the x and y distances
+                # show individual x & y distances next to drawn line
                 cv2.putText(image, f'X Distance: {x_distance:.2f}', (center[0], center[1] - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
                 cv2.putText(image, f'Y Distance: {y_distance:.2f}', (center[0], center[1] - 50), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
 
-                # If the Euclidean distance is less than a certain threshold, consider the point as visited
+                # if distance to point below threshold, consider point as visited
                 if time.time() - checkpoint_time >= 0.1:
                     close_points = np.where(euclidean_distances < 35)[0]
                     if close_points.size > 0:
                         first_close_point = close_points[0]
                         for i in range(first_close_point + 1):
                             cv2.circle(processed_image,(x_path[i], y_path[i]),3,(70,70,70),-1)
-                        # Remove the point from the path
                         x_path = np.delete(x_path, slice(0, first_close_point + 1))
                         y_path = np.delete(y_path, slice(0, first_close_point + 1))
                     checkpoint_time = time.time()
@@ -395,7 +302,8 @@ def run_image_processing():
                 dt = time.time() - interval_time
                 if dt > 0.01:
                     x_integral += x_distance * dt
-                    if x_integral > 500: #Saturation code for Ki values
+                    # set integration saturation vals to prevent windup
+                    if x_integral > 500:
                         x_integral = 500
                     elif x_integral < -500:
                         x_integral = -500
@@ -408,31 +316,23 @@ def run_image_processing():
                     y_derivative = (y_distance-y_distance_prev)/dt
                     PDx = PIDcontrol(KpX, KiX, KdX, x_distance, x_integral, x_derivative)
                     PDy = PIDcontrol(KpY, KiY, KdY, y_distance, y_integral, y_derivative)
-                    # print(x_distance_prev,y_distance_prev)
                     x_distance_prev = x_distance
                     y_distance_prev = y_distance
                     motorx = max(min(PDx, 60), -60)
                     motory = max(min(PDy, 60), -60)
-                        # Add a small random offset to the motor commands
-                    if x_derivative < 15 and y_derivative < 15:
-                        jitter = 8  # Adjust this value to change the amount of jitter
+                    # add jitter to reduce stick/slip friction
+                    if x_derivative < 10 and y_derivative < 10:
+                        jitter = 2.5
                         motorx += random.uniform(-jitter, jitter)
                         motory += random.uniform(-jitter, jitter)
-                    # print(x_distance,y_distance)
-                    # print(x_integral,y_integral)
-                    # print('de',x_derivative,y_derivative)
-                    # print(motorx,motory)
                     send_position(motorx,motory)
                     interval_time = time.time()
             
             cv2.imshow('Working Image', image)
 
-        #cv2.imshow('Video Feed', frame)
-
         if cv2.waitKey(1) & 0xFF == 27: #ESC key
             break
 
-    # Release the video capture and close windows
     cap.release()
     cv2.destroyAllWindows()
 
@@ -456,7 +356,6 @@ def send_position(angle1, angle2):
         angle2 = angle2 + 60
         ser.write(f"x,{angle1}".encode())
         ser.write(f"y,{angle2}".encode())
-        #print(f"Sent command: {angle1},{angle2}")
 
 def create_gui():
     global timer_label
